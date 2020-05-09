e6cf8d2e2e7efc32bd934c3c693999e90942cc02
Matt Windsor <m.windsor@imperial.ac.uk>
2020-05-09 13:22:31 +0100

    Update stats to new format

--- internal/act/stats.go
+++ internal/act/stats.go
@@ -10,6 +10,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/MattWindsor91/act-tester/internal/model/id"
 	"io"
 	"strconv"
 	"strings"
@@ -24,6 +25,30 @@ type StatDumper interface {
 	DumpStats(ctx context.Context, s *Statset, path string) error
 }
 
+// AtomicStatset contains a set of statistics about atomics (expressions or statements).
+type AtomicStatset struct {
+	// Types gives the types of atomic, categorised by type.
+	Types map[string]int
+	// MemOrders gives the types of memory order, categorised by type.
+	MemOrders map[string]int
+}
+
+// AddType adds k to the type with ID id.
+func (s *AtomicStatset) AddType(id id.ID, k int) {
+	if s.Types == nil {
+		s.Types = make(map[string]int)
+	}
+	s.Types[id.String()] += k
+}
+
+// AddMemOrder adds k to the memory order with ID id.
+func (s *AtomicStatset) AddMemOrder(id id.ID, k int) {
+	if s.MemOrders == nil {
+		s.MemOrders = make(map[string]int)
+	}
+	s.MemOrders[id.String()] += k
+}
+
 // Statset contains a set of statistics acquired from `act-c dump-stats`.
 type Statset struct {
 	// Threads is the number of threads.
@@ -35,17 +60,27 @@ type Statset struct {
 	// LiteralBools is the number of Boolean literals (true, false, etc).
 	LiteralBools int
 
-	// AtomicStatements is the number of atomic statements, categorised by type.
-	AtomicStatements map[string]int
+	// AtomicExpressions gives information about atomic statements.
+	AtomicExpressions AtomicStatset
+
+	// AtomicStatements gives information about atomic statements.
+	AtomicStatements AtomicStatset
 }
+
+const (
+	catAtomics           = "atomics"
+	catAtomicsExpression = "expression"
+	catAtomicsStatement  = "statement"
+	catLiterals          = "literals"
+	catLiteralsBool      = "bool"
+	catMemOrders         = "mem-orders"
+	catThreads           = "threads"
+	catReturns           = "returns"
+)
 
 // Parse parses a statistics set from r into this statistics set.
 // Each statistic should be in the form "name value\n".
 func (s *Statset) Parse(r io.Reader) error {
-	if s.AtomicStatements == nil {
-		s.AtomicStatements = make(map[string]int)
-	}
-
 	sc := bufio.NewScanner(r)
 	for sc.Scan() {
 		if err := s.parseLine(sc.Text()); err != nil {
@@ -66,30 +101,80 @@ func (s *Statset) parseLine(l string) error {
 	if err != nil {
 		return err
 	}
-	return s.setByName(name, val)
-}
-
-func (s *Statset) setByName(name string, val int) error {
-	frags := strings.Split(name, "-")
-	switch {
-	case len(frags) == 3 && frags[0] == "atomic" && frags[2] == "statements":
-		s.AtomicStatements[frags[1]] = val
-	case len(frags) == 2 && frags[0] == "literal" && frags[1] == "bools":
-		s.LiteralBools = val
-	case len(frags) == 1:
-		return s.setBySimpleName(name, val)
+	nid, err := id.TryFromString(name)
+	if err != nil {
+		return err
 	}
-	return nil
+	return s.setByID(nid, val)
 }
 
-func (s *Statset) setBySimpleName(name string, val int) error {
-	switch name {
-	case "threads":
+func errBadSubcategory(cat string, rest id.ID) error {
+	return fmt.Errorf("%w: unexpected subcategory for %q: %s", ErrStatsetParse, cat, rest)
+}
+
+func errEmptyId(cat string) error {
+	return fmt.Errorf("%w: empty %q sub-id", ErrStatsetParse, cat)
+}
+
+func (s *Statset) setByID(nid id.ID, val int) error {
+	ncat, nrest, ok := nid.Uncons()
+	if !ok {
+		return fmt.Errorf("%w: empty id", ErrStatsetParse)
+	}
+	switch ncat {
+	case catThreads:
+		if !nrest.IsEmpty() {
+			return errBadSubcategory(catThreads, nrest)
+		}
 		s.Threads = val
-	case "returns":
+	case catReturns:
+		if !nrest.IsEmpty() {
+			return errBadSubcategory(catReturns, nrest)
+		}
 		s.Returns = val
-	default:
-		return fmt.Errorf("%w: unknown stat %s", ErrStatsetParse, name)
+	case catAtomics:
+		return s.setAtomicType(nrest, val)
+	case catMemOrders:
+		return s.setMemOrder(nrest, val)
+	case catLiterals:
+		return s.setLiteral(nrest, val)
 	}
 	return nil
+}
+
+func (s *Statset) setLiteral(lid id.ID, val int) error {
+	lcat, lrest, ok := lid.Uncons()
+	if !ok {
+		return errEmptyId(catLiterals)
+	}
+	switch lcat {
+	case catLiteralsBool:
+		if !lrest.IsEmpty() {
+			return fmt.Errorf("%w: unexpected subcategory for '%s.%s': %s", ErrStatsetParse, catLiterals, catLiteralsBool, lrest)
+		}
+		s.LiteralBools = val
+	}
+	return nil
+}
+
+func setAtomicRelated(aid id.ID, val int, pcat string, exp, stm func(id.ID, int)) error {
+	cat, rest, ok := aid.Uncons()
+	if !ok {
+		return errEmptyId(pcat)
+	}
+	switch cat {
+	case catAtomicsExpression:
+		exp(rest, val)
+	case catAtomicsStatement:
+		stm(rest, val)
+	}
+	return nil
+}
+
+func (s *Statset) setAtomicType(tid id.ID, val int) error {
+	return setAtomicRelated(tid, val, catAtomics, s.AtomicExpressions.AddType, s.AtomicStatements.AddType)
+}
+
+func (s *Statset) setMemOrder(mid id.ID, val int) error {
+	return setAtomicRelated(mid, val, catMemOrders, s.AtomicExpressions.AddMemOrder, s.AtomicStatements.AddMemOrder)
 }
--- internal/act/stats_test.go
+++ internal/act/stats_test.go
@@ -14,27 +14,73 @@ import (
 )
 
 func TestStatset_Parse(t *testing.T) {
-	r := strings.NewReader(`threads 4
-returns 3
-literal-bools 34
-atomic-cmpxchg-statements 0
-atomic-fence-statements 3
-atomic-fetch-statements 2
-atomic-load-statements 0
-atomic-store-statements 2
-atomic-xchg-statements 2`)
+	r := strings.NewReader(`threads 3
+returns 0
+literals.bool 14
+atomics.expression.cmpxchg 0
+atomics.expression.fence 0
+atomics.expression.fetch 30
+atomics.expression.load 19
+atomics.expression.store 0
+atomics.expression.xchg 0
+mem-orders.expression.memory_order_relaxed 15
+mem-orders.expression.memory_order_consume 4
+mem-orders.expression.memory_order_acquire 11
+mem-orders.expression.memory_order_release 10
+mem-orders.expression.memory_order_acq_rel 3
+mem-orders.expression.memory_order_seq_cst 6
+atomics.statement.cmpxchg 0
+atomics.statement.fence 1
+atomics.statement.fetch 2
+atomics.statement.load 0
+atomics.statement.store 0
+atomics.statement.xchg 3
+mem-orders.statement.memory_order_relaxed 8
+mem-orders.statement.memory_order_consume 0
+mem-orders.statement.memory_order_acquire 0
+mem-orders.statement.memory_order_release 7
+mem-orders.statement.memory_order_acq_rel 0
+mem-orders.statement.memory_order_seq_cst 0`)
 
 	want := Statset{
-		Threads:      4,
-		Returns:      3,
-		LiteralBools: 34,
-		AtomicStatements: map[string]int{
-			"cmpxchg": 0,
-			"fence":   3,
-			"fetch":   2,
-			"load":    0,
-			"store":   2,
-			"xchg":    2,
+		Threads:      3,
+		Returns:      0,
+		LiteralBools: 14,
+		AtomicExpressions: AtomicStatset{
+			Types: map[string]int{
+				"cmpxchg": 0,
+				"fence":   0,
+				"fetch":   30,
+				"load":    19,
+				"store":   0,
+				"xchg":    0,
+			},
+			MemOrders: map[string]int{
+				"memory_order_relaxed": 15,
+				"memory_order_consume": 4,
+				"memory_order_acquire": 11,
+				"memory_order_release": 10,
+				"memory_order_acq_rel": 3,
+				"memory_order_seq_cst": 6,
+			},
+		},
+		AtomicStatements: AtomicStatset{
+			Types: map[string]int{
+				"cmpxchg": 0,
+				"fence":   1,
+				"fetch":   2,
+				"load":    0,
+				"store":   0,
+				"xchg":    3,
+			},
+			MemOrders: map[string]int{
+				"memory_order_relaxed": 8,
+				"memory_order_consume": 0,
+				"memory_order_acquire": 0,
+				"memory_order_release": 7,
+				"memory_order_acq_rel": 0,
+				"memory_order_seq_cst": 0,
+			},
 		},
 	}
 
