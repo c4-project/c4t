2bb0649e20f7e29500c3b248fe832ed0ce248948
Matt Windsor <m.windsor@imperial.ac.uk>
2020-07-24 16:17:54 +0100

    Hook CSV up to analyser
    Use -csv-compilers to get compiler CSV.

--- internal/app/analyse/analyse.go
+++ internal/app/analyse/analyse.go
@@ -6,8 +6,11 @@
 package analyse
 
 import (
+	"encoding/csv"
 	"io"
 	"io/ioutil"
+
+	csv2 "github.com/MattWindsor91/act-tester/internal/stage/analyser/csv"
 
 	"github.com/MattWindsor91/act-tester/internal/stage/analyser/pretty"
 
@@ -38,6 +41,8 @@ const (
    summary of the plan file.  By passing -` + flagSaveDir + `, one can
    archive failing corpora to a directory for later experimentation.`
 
+	flagCsvCompilers       = "csv-compilers"
+	usageCsvCompilers      = "dump CSV of compilers and their run times"
 	flagShowCompilers      = "show-compilers"
 	flagShowCompilersShort = "C"
 	usageShowCompilers     = "show breakdown of compilers and their run times"
@@ -70,6 +75,7 @@ func App(outw, errw io.Writer) *c.App {
 func flags() []c.Flag {
 	return []c.Flag{
 		stdflag.WorkerCountCliFlag(),
+		&c.BoolFlag{Name: flagCsvCompilers, Usage: usageCsvCompilers},
 		&c.BoolFlag{Name: flagShowCompilers, Aliases: []string{flagShowCompilersShort}, Usage: usageShowCompilers},
 		&c.BoolFlag{Name: flagShowOk, Aliases: []string{flagShowOkShort}, Usage: usageShowOk},
 		&c.BoolFlag{Name: flagShowSubjects, Aliases: []string{flagShowSubjectsShort}, Usage: usageShowSubjects},
@@ -85,19 +91,13 @@ func flags() []c.Flag {
 }
 
 func run(ctx *c.Context, outw io.Writer, _ io.Writer) error {
-	obs, err := pretty.NewPrinter(
-		pretty.WriteTo(outw),
-		pretty.ShowCompilers(ctx.Bool(flagShowCompilers)),
-		pretty.ShowOk(ctx.Bool(flagShowOk)),
-		pretty.ShowSubjects(ctx.Bool(flagShowSubjects)),
-		pretty.ShowPlanInfo(ctx.Bool(flagShowPlanInfo)),
-	)
+	obs, err := observers(ctx, outw)
 	if err != nil {
 		return err
 	}
 
 	a, err := analyser.New(
-		analyser.ObserveWith(obs),
+		analyser.ObserveWith(obs...),
 		analyser.ParWorkers(stdflag.WorkerCountFromCli(ctx)),
 		analyser.SaveToPathset(savedPaths(ctx)),
 	)
@@ -105,6 +105,42 @@ func run(ctx *c.Context, outw io.Writer, _ io.Writer) error {
 		return err
 	}
 	return view.RunOnCliPlan(ctx, a, ioutil.Discard)
+}
+
+func observers(ctx *c.Context, outw io.Writer) ([]analyser.Observer, error) {
+	obs, err := prettyObserver(ctx, outw)
+	if err != nil {
+		return nil, err
+	}
+	return csvObserver(ctx, outw, obs)
+}
+
+func prettyObserver(ctx *c.Context, outw io.Writer) ([]analyser.Observer, error) {
+	showCompilers := ctx.Bool(flagShowCompilers)
+	showOk := ctx.Bool(flagShowOk)
+	showSubjects := ctx.Bool(flagShowSubjects)
+	showPlanInfo := ctx.Bool(flagShowPlanInfo)
+
+	if showCompilers || showOk || showSubjects || showPlanInfo {
+		po, err := pretty.NewPrinter(
+			pretty.WriteTo(outw),
+			pretty.ShowCompilers(ctx.Bool(flagShowCompilers)),
+			pretty.ShowOk(ctx.Bool(flagShowOk)),
+			pretty.ShowSubjects(ctx.Bool(flagShowSubjects)),
+			pretty.ShowPlanInfo(ctx.Bool(flagShowPlanInfo)),
+		)
+		return []analyser.Observer{po}, err
+	}
+	return nil, nil
+}
+
+func csvObserver(ctx *c.Context, outw io.Writer, obs []analyser.Observer) ([]analyser.Observer, error) {
+	showCsvCompilers := ctx.Bool(flagCsvCompilers)
+
+	if showCsvCompilers {
+		return append(obs, (*csv2.CompilerWriter)(csv.NewWriter(outw))), nil
+	}
+	return obs, nil
 }
 
 func savedPaths(ctx *c.Context) *saver.Pathset {
