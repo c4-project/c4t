cdac83f9295337dea4048a84b449ec4e0b73946a
Matt Windsor <m.windsor@imperial.ac.uk>
2020-08-13 16:52:00 +0100

    Fuzz: re-introduce config file quantities
    These had been accidentally removed at some point, probably because I thought it
    would be better if the fuzzer had no dependencies on the configuration system.
    However, this prevents the config file from specifying default quantities in the
    single-shot setup, so I've re-introduced them.

--- internal/app/fuzz/fuzz.go
+++ internal/app/fuzz/fuzz.go
@@ -10,6 +10,8 @@ import (
 	"io"
 	"log"
 
+	"github.com/MattWindsor91/act-tester/internal/config"
+
 	"github.com/MattWindsor91/act-tester/internal/stage/fuzzer"
 
 	"github.com/MattWindsor91/act-tester/internal/ux/singleobs"
@@ -39,6 +41,7 @@ func App(outw, errw io.Writer) *c.App {
 
 func flags() []c.Flag {
 	fs := []c.Flag{
+		stdflag.ConfFileCliFlag(),
 		stdflag.OutDirCliFlag(defaultOutDir),
 		stdflag.CorpusSizeCliFlag(),
 		stdflag.SubjectCyclesCliFlag(),
@@ -47,21 +50,27 @@ func flags() []c.Flag {
 }
 
 func run(ctx *c.Context, outw, errw io.Writer) error {
+	cfg, err := stdflag.ConfFileFromCli(ctx)
+	if err != nil {
+		return err
+	}
+
 	a := stdflag.ActRunnerFromCli(ctx, errw)
 	l := log.New(errw, "", 0)
-	f, err := makeFuzzer(ctx, a, l)
+	f, err := makeFuzzer(ctx, cfg, a, l)
 	if err != nil {
 		return err
 	}
 	return ux.RunOnCliPlan(ctx, f, outw)
 }
 
-func makeFuzzer(ctx *c.Context, drv fuzzer.Driver, l *log.Logger) (*fuzzer.Fuzzer, error) {
+func makeFuzzer(ctx *c.Context, cfg *config.Config, drv fuzzer.Driver, l *log.Logger) (*fuzzer.Fuzzer, error) {
 	return fuzzer.New(
 		drv,
 		fuzzer.NewPathset(stdflag.OutDirFromCli(ctx)),
 		fuzzer.LogWith(l),
 		fuzzer.ObserveWith(singleobs.Builder(l)...),
+		fuzzer.OverrideQuantities(cfg.Quantities.Fuzz),
 		fuzzer.OverrideQuantities(setupQuantityFlags(ctx)),
 	)
 }
