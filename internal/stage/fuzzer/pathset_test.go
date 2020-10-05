8830544ec77a53561b417d2cd5f383e6308a8fe7
Matt Windsor <m.windsor@imperial.ac.uk>
2020-10-05 14:17:17 +0100

    Fuzzer: add prepare tests
    We have tempdir support in Go 1.15, so this is feasible now.  Other
    pathset prepare tests will follow as and when I realise they're missing!

---
+++ internal/stage/fuzzer/pathset_test.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2020 Matt Windsor and contributors
+//
+// This file is part of act-tester.
+// Licenced under the MIT licence; see `LICENSE`.
+
+package fuzzer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TestPathset_Prepare tests Pathset.Prepare in a temporary directory.
+func TestPathset_Prepare(t *testing.T) {
+	td := t.TempDir()
+	ps := NewPathset(td)
+
+	assert.NoDirExists(t, ps.DirLitmus)
+	assert.NoDirExists(t, ps.DirTrace)
+
+	err := ps.Prepare()
+	require.NoError(t, err, "preparing fuzzer pathset in temp dir")
+
+	assert.DirExists(t, ps.DirLitmus)
+	assert.DirExists(t, ps.DirLitmus)
+}
